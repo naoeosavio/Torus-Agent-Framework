1e59f2158bcc03eae4939dccfab5b8b5c8b4d2b7
NaoEhSavio <savioscmendes@gmail.com>
2025-02-26 16:17:23 -0300

    add types

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+// Define newtype/branded types
+
+import { z } from './';
+
+export type CID = string & { readonly __brand: unique symbol };
+export const CID = z.string().transform((val) => val as CID);
+
+export type SS58Address = string & { readonly __brand: unique symbol };
+export const SS58Address = z.string().transform((val) => val as SS58Address);
+
+
